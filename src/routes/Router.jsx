d3717267af1c12139cdc31f80c40fc877fafe63d
vpvm96 <vpvm96@gmail.com>
2022-12-22 12:03:58 +0900

    fix. add router detail path

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import TodoListPage from "../pages/TodoListPage"
 import ErrorPage from "../pages/ErrorPage"
+import TodoListDetailPage from "../pages/TodoListDetailPage"
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TodoListPage />} />
+        <Route path="/todo/:todoId" element={<TodoListDetailPage />} />
         <Route path={"*"} element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
